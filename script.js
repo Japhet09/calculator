a9eeb9c9125efab7acc172d9a2337a7952585c6a
Japhet09 <japhetalfred19@gmail.com>
2022-01-03 22:22:21 +0100

    added all the numbers and operators

--- script.js
+++ script.js
@@ -7,10 +7,35 @@ display.classList.add('display')
 display.innerText = 0;
 container.appendChild(display)
 
+//create clear button
+const clear = document.createElement('div')
+clear.classList.add('clear')
+clear.innerText = 'CLEAR'
+container.appendChild(clear)
+
 //Create calculator numbers
 for(i=9;i>=0;i--){
     const num= document.createElement('div')
     num.classList.add('number')
     num.innerText = `${i}`
     container.appendChild(num)
+}
+//Create decimal point  
+const decimal = document.createElement('div')
+decimal.classList.add('decimal')
+decimal.innerText = '.'
+container.appendChild(decimal)
+
+
+
+// A list of operators
+const operators = ['/', 'x', '-','+', '=']
+
+// create element for each operator and append to the calculator
+
+for(i=0;i<operators.length;i++){
+    const operator = document.createElement('div')
+    operator.classList.add('operator', `operator${operators[i]}`)
+    operator.innerText = `${operators[i]}`
+    container.appendChild(operator)
 }
